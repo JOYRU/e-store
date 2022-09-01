b9d2312fc90819f82141b91a6de025d3d42ed792
joy <joycseru@gmail.com>
2022-09-01 13:15:12 +0600

    local strategy update

--- views/utils/authStrategy/localStrategy.js
+++ views/utils/authStrategy/localStrategy.js
@@ -4,25 +4,13 @@ const LocalStrategy = require('passport-local').Strategy
 
 passport.use(new LocalStrategy(
     async(email,password,done)=>{
-
+     try{
        const user = await User.findOne({email})
-        try{
-
+       return done(null,user)
         
-        //     ,function(err,user){
-        //     if(err){ return done(err)}
-        //     if(!user){
-        //         return done(null,false,{message:'Incorrect username.'})
-        //     }
-        //     if(!user.validPassword(password)){
-        //         return done(null,false,{message:'Incorrect password'})
-
-        //     }
-           return done(null,user)
-        // })
-        }catch{
+        }catch(e){
             done(e)
         }
     }
   )
-)
+) 
